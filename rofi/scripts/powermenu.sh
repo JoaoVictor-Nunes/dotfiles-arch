a311ce8d0f94d4cb3571d4e7cfdc0c3966009d34
joao-unichristus <joao241000954@gmail.com>
2026-01-21 23:13:59 -0300

    rofi power menu atualizado

--- rofi/scripts/powermenu.sh
+++ rofi/scripts/powermenu.sh
@@ -3,10 +3,9 @@
 # Current Theme
 theme="$HOME/.config/rofi/styles/powermenu.rasi"
 
-
 # CMDs
 uptime="$(uptime -p | sed -e 's/up //g')"
-host=$(hostname)
+host=$(cat /proc/sys/kernel/hostname 2>/dev/null || echo "localhost")
 
 # Options
 shutdown='󰐥  Shutdown'
@@ -19,70 +18,71 @@ no='  No'
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "Uptime: $uptime" \
+    rofi -dmenu \
+        -p "$host" \
+        -mesg "Uptime: $uptime" 
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+        -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+        -theme-str 'listview {columns: 2; lines: 1;}' \
+        -theme-str 'element-text {horizontal-align: 0.5;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?'
 }
 
 # Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
+    echo -e "$yes\n$no" | confirm_cmd
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+    echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--lock' ]]; then
-			$HOME/.config/hypr/scripts/lock
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			hyprctl dispatch exit
-		fi
-	else
-		exit 0
-	fi
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--lock' ]]; then
+            $HOME/.config/hypr/scripts/lock
+        elif [[ $1 == '--suspend' ]]; then
+            mpc -q pause
+            amixer set Master mute
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            hyprctl dispatch exit
+        fi
+    else
+        exit 0
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-$shutdown)
-	run_cmd --shutdown
-	;;
-$reboot)
-	run_cmd --reboot
-	;;
-$lock)
-	$HOME/.config/hypr/scripts/lock
-	;;
-$suspend)
-	run_cmd --suspend
-	;;
-$logout)
-	run_cmd --logout
-	;;
+    $shutdown)
+        run_cmd --shutdown
+        ;;
+    $reboot)
+        run_cmd --reboot
+        ;;
+    $lock)
+        $HOME/.config/hypr/scripts/lock
+        ;;
+    $suspend)
+        run_cmd --suspend
+        ;;
+    $logout)
+        run_cmd --logout
+        ;;
 esac
